9adfb19f4b9e76eb9f54984a8c3ff1672b95b475
Hussam ElNemr <hassom20042019@gmail.com>
2026-02-07 05:33:00 +0200

    Update Docker Commands

--- Docker-Commands.bash
+++ Docker-Commands.bash
@@ -2,5 +2,5 @@
 docker run -d   --name some-redis   -p 6379:6379   redis redis-server --save 60 1 --loglevel warning
 docker run -d   -p 3000:3000   --name=grafana   -v grafana_data:/var/lib/grafana   grafana/grafana-enterprise
 # Run after created
-docker run -d -p 6379:6379 redis
-docker run -d -p 3000:3000 grafana/grafana-enterprise
+docker start some-redis
+docker start grafana
